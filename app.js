bc0ba772249c75a85036ca0abffeb5b7ef3362a8
winterwurzel <georg.markowitsch@gmail.com>
2017-04-01 16:18:08 +0200

    demo routes

--- app.js
+++ app.js
@@ -3,6 +3,33 @@ var app = express();
 var dbtools = require('./db.js');
 var Snippet = require('./snippet.js');
 
+app.get('/snippets', function (req, res) {
+  console.log("test" + {});
+  if (Object.is(req.query, {})) {
+    res.send('GET request to the homepage');
+  }
+  else {
+    res.send('GET request with query: ' + req.query);
+  }
+});
+
+app.post('/snippets', function (req, res) {
+  res.send('POST request to the homepage');
+});
+
+app.get('/snippets/:id', function (req, res) {
+  res.send('GET request to the id: ' + req.params.id);
+});
+
+app.put('/snippets/:id', function (req, res) {
+  res.send('PUT request to the id: ' + req.params.id);
+});
+
+app.delete('/snippets/:id', function (req, res) {
+  res.send('DELETE request to the id: ' + req.params.id);
+});
+
+
 app.get('/', function (req, res) {
   var snip = new Snippet("1", "2", "3", "4", "5", "6");
   console.log(snip.getJson());
