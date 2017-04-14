e451ac2026d7ee37c7c82dc8fa84aa936fb6b7db
Georg Markowitsch <g.markowitsch@netconomy.net>
2017-04-14 18:00:32 +0200

    Put fix

--- db.js
+++ db.js
@@ -20,9 +20,9 @@ module.exports = {
       callback(data);
       return true;
     })
-    .catch(function (error) {
+    .catch(function (errorMsg) {
       error(res, 400, "something went wrong");
-      return error;
+      return errorMsg;
     })
   },
 
@@ -33,9 +33,9 @@ module.exports = {
         callback(data);
         return true;
       })
-      .catch(error => {
+      .catch(errorMsg => {
         error(res, 400, "something went wrong");
-        return error;
+        return errorMsg;
       });
   },
 
@@ -51,14 +51,17 @@ module.exports = {
       snippet.language = '%';
     if (snippet.code == null)
       snippet.code = '%';
-    db.any('select * from snippets where (($1 IS NULL) OR (ID = $1)) and name like $2 and description like $3 and author like $4 and language like $5 and code like $6 and (($7 IS NULL) OR (tags @> array[$7]))', [snippet.id, snippet.name, snippet.description, snippet.author, snippet.language, snippet.code, snippet.tags])
+    db.any('select * from snippets where (($1 IS NULL) OR (ID = $1)) and name like $2 \
+        and description like $3 and author like $4 and language like $5 \
+        and code like $6 and (($7 IS NULL) OR (tags @> array[$7]))',
+        [snippet.id, snippet.name, snippet.description, snippet.author, snippet.language, snippet.code, snippet.tags])
       .then(data => {
         callback(data);
         return true;
       })
-      .catch(error => {
+      .catch(errorMsg => {
         error(res, 400, "something went wrong");
-        return error;
+        return errorMsg;
       });
   },
 
@@ -68,9 +71,9 @@ module.exports = {
       callback(data);
       return true;
     })
-    .catch(function (error) {
+    .catch(function (errorMsg) {
       error(res, 400, "something went wrong");
-      return error;
+      return errorMsg;
     })
   },
 
@@ -80,23 +83,50 @@ module.exports = {
       callback();
       return true;
     })
-    .catch(function (error) {
+    .catch(function (errorMsg) {
       error(res, 400, "something went wrong");
-      return error;
+      return errorMsg;
     })
   },
 
-  putWithId: function(snippet, callback, error, res) {
-    console.log(snippet.getJson());
-    db.none('update snippets set name=$1, description=$2, author=$3, language=$4, code=$5, tags=$6 where id=$7', [snippet.name, snippet.description, snippet.author, snippet.language, snippet.code, snippet.tags, snippet.id])
+  putWithId: function(snippetOrig, callback, error, res) {
+     var updateCallback = function(snippet) {
+        if (snippetOrig.name != null)
+          snippet.name = snippetOrig.name;
+        if (snippetOrig.description != null)
+          snippet.description = snippetOrig.description;
+        if (snippetOrig.author != null)
+          snippet.author = snippetOrig.author;
+        if (snippetOrig.language != null)
+          snippet.language = snippetOrig.language;
+        if (snippetOrig.code != null)
+          snippet.code = snippetOrig.code;
+        if (snippetOrig.tags != null)
+          snippet.tags = snippetOrig.tags;
+        console.log(JSON.stringify(snippet));
+        db.none('update snippets set name=$1, description=$2, author=$3, \
+          language=$4, code=$5, tags=$6 where id=$7',
+          [snippet.name, snippet.description, snippet.author, snippet.language, snippet.code, snippet.tags, snippet.id])
+        .then(() => {
+          callback();
+          return true;
+        })
+        .catch(function (error) {
+          error(res, 400, "something went wrong");
+          return error;
+        })
+     }
+
+
+
+    db.one('select * from snippets where id=$1', [snippetOrig.id])
     .then(data => {
-      console.log(data);
-      callback();
+      updateCallback(data);
       return true;
     })
-    .catch(function (error) {
-      error(res, 400, "something went wrong");
-      return error;
+    .catch(function (errorMsg) {
+      error(res, 400, "something went wrong, " + errorMsg);
+      return errorMsg;
     })
   }
 }
